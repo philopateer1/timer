4c5c6f099096dac1a48d4886f41faae08afffac0
philopateer1 <68166714+philopateer1@users.noreply.github.com>
2025-05-12 20:31:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const btn = document.getElementById("submit");
+const reset = document.getElementById("reset");
+const pause = document.getElementById("pause");
+const inputSec = document.getElementById("inputSec");
+const inputMin = document.getElementById("inputMin");
+const inputHr = document.getElementById("inputHr");
+
+btn.addEventListener("click", () => {
+    let timeSec = parseInt(inputSec.value, 10) || 0;
+    let timeMin = parseInt(inputMin.value, 10) || 0;
+    let timeHr = parseInt(inputHr.value, 10) || 0;
+
+    const intervalId = setInterval(() => {
+        if (timeSec === 0) {
+            if (timeMin === 0) {
+                if (timeHr === 0) {
+                    alert("Time's Up!");
+                    clearInterval(intervalId);
+                    inputSec.value = 0;
+                    inputMin.value = 0;
+                    inputHr.value = 0;
+                    return;
+                } else {
+                    timeHr--;
+                    timeMin = 59;
+                    timeSec = 59;
+                }
+            } else {
+                timeMin--;
+                timeSec = 59;
+            }
+        } else {
+            timeSec--;
+        }
+
+        inputSec.value = timeSec;
+        inputMin.value = timeMin;
+        inputHr.value = timeHr;
+        pause.addEventListener("click",()=>{
+            clearInterval(intervalId);
+        });
+        reset.addEventListener("click",()=>{
+            clearInterval(intervalId);
+            inputSec.value = 0;
+            inputMin.value = 0;
+            inputHr.value = 0;
+        })
+    }, 1000);
+});
